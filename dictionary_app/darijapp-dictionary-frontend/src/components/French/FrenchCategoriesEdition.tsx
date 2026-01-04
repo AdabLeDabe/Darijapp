6e39ba5b3916d9155214a15c4ab521d48f0528cc
Adam Akkari <adam.akkari@epitech.eu>
2026-01-04 23:38:55 +0100

    Implemented basic selection when editing categories for french word

--- dictionary_app/darijapp-dictionary-frontend/src/components/French/FrenchCategoriesEdition.tsx
+++ dictionary_app/darijapp-dictionary-frontend/src/components/French/FrenchCategoriesEdition.tsx
@@ -31,12 +31,28 @@ function FrenchCategoriesEdition({ frenchId, currentlySelectedCategories }: Fren
         setSelectedCategories(currentlySelectedCategories);
     }, [frenchId]);
 
+    const toggleSelection = (category: Category) => {
+        setSelectedCategories(oldArray => {
+            if (isCategorySelected(category)) {
+                return oldArray.filter(selected => selected.id !== category.id);
+            }
+            else {
+                return [...oldArray, category];
+            }
+        });
+    }
+
+    const isCategorySelected = (category: Category) => {
+        return selectedCategories.some(selected => selected.id === category.id);
+    }
+
     return (
         <div className="sub-container">
             <div className="category-list">
                 {categoriesList.map(category => (
                     <div
-                        key={category.id} >
+                        key={category.id}
+                        onClick={() => toggleSelection(category)} >
                         <WordDisplay isSelected={selectedCategories.some(selectedCategory => selectedCategory.id == category.id)}>
                             {category.category_name}
                         </WordDisplay>
