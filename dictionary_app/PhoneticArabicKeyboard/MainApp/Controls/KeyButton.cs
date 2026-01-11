360b5aa12c1293d6b19f974230051396ac54b860
Adam Akkari <adam.akkari@epitech.eu>
2026-01-11 11:22:50 +0100

    Implemented basic keyboard layout for phonetic arabic keyboard

---
+++ dictionary_app/PhoneticArabicKeyboard/MainApp/Controls/KeyButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MainApp.Controls
+{
+    public class KeyButton : Button
+    {
+        public static readonly DependencyProperty KeyCharProperty =
+            DependencyProperty.Register(
+                "KeyChar",
+                typeof(char),
+                typeof(KeyButton),
+                new PropertyMetadata(default(char), OnKeyCharChanged));
+
+        public char KeyChar
+        {
+            get => (char)GetValue(KeyCharProperty);
+            set => SetValue(KeyCharProperty, value);
+        }
+
+        private static void OnKeyCharChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is KeyButton keyButton)
+            {
+                keyButton.Content = keyButton.KeyChar.ToString();
+            }
+        }
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+            // Call your input-sending logic here, e.g. InputSender.SendKey(KeyChar);
+        }
+    }
+}
