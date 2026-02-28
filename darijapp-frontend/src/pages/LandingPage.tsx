78b498e423422d60d0f816aaec322813037c3cc8
Adam Akkari <adam.akkari@epitech.eu>
2026-02-28 14:53:02 +0100

    Added landing page with proper responsive layout and all that

--- darijapp-frontend/src/pages/LandingPage.tsx
+++ darijapp-frontend/src/pages/LandingPage.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import ToggleButtons from "../components/ToggleButtons";
 import "../styles/LandingPage.css"
+import AppButton from "../components/AppButton";
 
 function LandingPage() {
+    //Temporary states for testing
+    const [selectedDialect, setSelectedDialect] = useState<string>("");
+
     return (
         <>
             <div className="landing-page-container">
-                <div className="title-label">test</div>
-                <div className="dialect-selection">test</div>
-                <div className="start-button">test</div>
+                <div className="title-label">
+                    <h2>Enrichissez votre vocabulaire en darija !</h2>
+                </div>
+                <div className="dialect-selection">
+                    <div className="label">Sélectionnez un pays pour privilégier le dialecte spécifique à celui-ci:</div>
+                    <ToggleButtons
+                        selectionChangedCallback={setSelectedDialect}
+                        option1='Maroc'
+                        option2='Algérie'
+                        option3='Tunisie' />
+                </div>
+                <div className="start-button">
+                    <AppButton isPrimary={true} isBig={true} hasArrow={true}>Commencer un quiz</AppButton>
+                </div>
             </div>
         </>
     )
