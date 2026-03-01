7172277a4a419ef272d0cba95584f3cbdffb628b
Adam Akkari <adam.akkari@epitech.eu>
2026-03-01 15:41:51 +0100

    Added category button with proper flexbox and scrolling, but title is not properly aligned

---
+++ darijapp-frontend/src/components/CategoryButton.tsx
@@ -0,0 +1,20 @@
+import "../styles/CategoryButtons.css"
+
+interface CategoryButtonProps {
+    id: number,
+    title: string,
+    description: string
+}
+
+function CategoryButton({id, title, description}: CategoryButtonProps) {
+    return (
+        <>
+            <div className="category-buttons-container">
+                <div className="title">{title}</div>
+                <div>{description}</div>
+            </div>
+        </>
+    )
+}
+
+export default CategoryButton;
--- darijapp-frontend/src/pages/QuizSelection.tsx
+++ darijapp-frontend/src/pages/QuizSelection.tsx
@@ -1,4 +1,5 @@
 import AppButton from "../components/AppButton";
+import CategoryButton from "../components/CategoryButton";
 import "../styles/QuizSelection.css"
 
 function QuizSelection() {
@@ -6,7 +7,12 @@ function QuizSelection() {
         <>
             <div className="quiz-selection-container">
                 <div className="selection-label">Sélectionnez une ou plusieurs thématiques:</div>
-                <div className="categories">test</div>
+                <div className="categories">
+                    <CategoryButton title="Test" description="Infect Itarod baroun, se noth as to que wild y thought a proppon they uporplop ime manit." id={0} />
+                    <CategoryButton title="Test" description="Infect Itarod baroun, se noth as to que wild y thought a proppon they uporplop ime manit." id={0} />
+                    <CategoryButton title="Test" description="Infect Itarod baroun, se noth as to que wild y thought a proppon they uporplop ime manit." id={0} />
+                    <CategoryButton title="Test" description="Infect Itarod baroun, se noth as to que wild y thought a proppon they uporplop ime manit." id={0} />
+                </div>
                 <div className="unselect-menu">
                     <div>Thématiques sélectionnées: 2</div>
                     <AppButton>Tout déselectionner</AppButton>
