8e51390695ae231be404d46a88903264c72d0a38
Adam Akkari <adam.akkari@epitech.eu>
2026-03-14 23:24:34 +0100

    Added basic navbar menu

--- darijapp-frontend/src/components/Navbar.tsx
+++ darijapp-frontend/src/components/Navbar.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import menuIcon from "../assets/menu.svg"
 import "../styles/Navbar.css"
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+    const displayMenu = () => {
+        if (!menuOpen)
+            return <></>
+
+        return (
+            <>
+                <div className="menu-list">
+                    <div className="menu-list-element">Commencer un quiz</div>
+                    <div className="menu-list-element">Paramètres</div>
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
-            <header className="navbar">
-                <button className="menu-btn" type="button">
+            <div className="navbar">
+                <button className="menu-btn" type="button" onClick={() => setMenuOpen(!menuOpen)}>
                     <img src={menuIcon} alt="Menu" aria-hidden="true" className="menu-btn-icon" />
                 </button>
                 <h1>Darijapp</h1>
-            </header>
+            </div>
+            {displayMenu()}
         </>
     )
 }
