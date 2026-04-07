e75f98f70de42c6e197141b1909031573de64315
Adam Akkari <adam.akkari@epitech.eu>
2026-04-07 21:58:39 +0200

    Added very basic quiz results page to finish whole quiz process (finally)

--- darijapp-frontend/src/pages/QuizProcess.tsx
+++ darijapp-frontend/src/pages/QuizProcess.tsx
@@ -3,31 +3,48 @@ import type { Answer, Question } from "../models/Question";
 import { GetQuestions } from "../helpers/ApiHelper";
 import QuizSelection from "./QuizSelection";
 import QuizQuestion from "./QuizQuestion";
-import type { QuestionResult } from "../models/QuizResult";
+import type { QuestionResult } from "../models/QuestionResult";
+import { QuizResults } from "./QuizResults";
 
 export function QuizProcess() {
     const [questions, setQuestions] = useState<Question[]>([]);
     const [currentQuestionId, setCurrentQuestionId] = useState<number>(0);
-    const quizResults: QuestionResult[] = [];
+    const [questionResults, setQuestionsResults] = useState<QuestionResult[]>([]);
+    const [quizDone, setQuizDone] = useState<boolean>(false);
 
     const categoriesSelected = (selectedCategories: number[]) => {
         setQuestions(GetQuestions(selectedCategories));
     }
 
     const goToNextQuestion = (selectedAnswer: Answer, correctAnswer: Answer) => {
-        quizResults.push({
+        questionResults.push({
             question: questions[currentQuestionId],
             selectedAnswer: selectedAnswer,
             correctAnswer: correctAnswer
         })
-        setCurrentQuestionId(currentQuestionId + 1);
+        if (currentQuestionId >= questions.length - 1) {
+            setQuizDone(true);
+        }
+        else {
+            setCurrentQuestionId(currentQuestionId + 1);
+        }
+    }
+
+    const restartQuiz = () => {
+        setQuestions([]);
+        setQuestionsResults([]);
+        setCurrentQuestionId(0);
+        setQuizDone(false);
     }
 
     return (
         <>
             {questions.length == 0
                 ? <QuizSelection categoriesSelectedCallback={categoriesSelected} />
-                : <QuizQuestion question={questions[currentQuestionId]} nextQuestionCallback={goToNextQuestion} />}
+                : quizDone
+                    ? <QuizResults questionResults={questionResults} restartQuizCallback={restartQuiz} />
+                    : <QuizQuestion question={questions[currentQuestionId]} nextQuestionCallback={goToNextQuestion} />
+            }
         </>
     );
 }
---
+++ darijapp-frontend/src/pages/QuizResults.tsx
@@ -0,0 +1,40 @@
+import "../styles/QuizResults.css"
+import type { QuestionResult } from "../models/QuestionResult"
+import AppButton from "../components/AppButton"
+
+interface QuizResultsProps {
+    questionResults: QuestionResult[],
+    restartQuizCallback: () => void
+}
+
+export function QuizResults({ questionResults, restartQuizCallback }: QuizResultsProps) {
+
+    const getGoodAnswersNumber = () => {
+        let number = 0;
+        questionResults.forEach(questionResult => {
+            if (questionResult.correctAnswer.id === questionResult.selectedAnswer.id) {
+                number++;
+            }
+        });
+        return number;
+    }
+
+    return (
+        <>
+            <div className="quiz-results-container">
+                <div className="title">
+                    <h2>Terminé !</h2>
+                </div>
+                <div className="results">Vous avez obtenu {getGoodAnswersNumber()}/{questionResults.length}</div>
+                <div className="results-details"></div>
+                <div className="restart">
+                    <AppButton
+                        isPrimary={true}
+                        isBig={true}
+                        hasArrow={true}
+                        onClick={() => restartQuizCallback()}>Commencer un nouveau quiz</AppButton>
+                </div>
+            </div>
+        </>
+    )
+}
